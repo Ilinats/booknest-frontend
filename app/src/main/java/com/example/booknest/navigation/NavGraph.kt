a16ce695d144b8f9ff27f45f3f9ad832297e08e8
Ilina Tsanova <107075825+Ilinats@users.noreply.github.com>
2025-09-22 11:14:54 +0300

    feat: navigation graph and screens

---
+++ app/src/main/java/com/example/booknest/navigation/NavGraph.kt
@@ -0,0 +1,50 @@
+package com.example.booknest.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.booknest.ui.account.AccountTypeScreen
+import com.example.booknest.ui.account.BioScreen
+import com.example.booknest.ui.account.GenresScreen
+import com.example.booknest.ui.account.LandingScreen
+import com.example.booknest.ui.account.LoginScreen
+import com.example.booknest.ui.account.PersonalInfoScreen
+import com.example.booknest.ui.account.ProfileDetailsScreen
+import com.example.booknest.ui.home.HomeScreen
+import com.example.booknest.viewmodel.LoginViewModel
+import com.example.booknest.viewmodel.SignupViewModel
+
+@Composable
+fun NavGraph(
+    navController: NavHostController,
+    signupViewModel: SignupViewModel,
+    loginViewModel: LoginViewModel
+) {
+    NavHost(navController, startDestination = Screen.Landing.route) {
+        composable(Screen.Landing.route) {
+            LandingScreen(navController)
+        }
+        composable(Screen.Login.route) {
+            LoginScreen(navController, loginViewModel)
+        }
+        composable(Screen.AccountType.route) {
+            AccountTypeScreen(navController, signupViewModel)
+        }
+        composable(Screen.PersonalInfo.route) {
+            PersonalInfoScreen(navController, signupViewModel)
+        }
+        composable(Screen.ProfileDetails.route) {
+            ProfileDetailsScreen(navController, signupViewModel)
+        }
+        composable(Screen.Bio.route) {
+            BioScreen(navController, signupViewModel)
+        }
+        composable(Screen.Genres.route) {
+            GenresScreen(navController, signupViewModel)
+        }
+        composable(Screen.Home.route) {
+            HomeScreen()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/booknest/navigation/Screen.kt
@@ -0,0 +1,12 @@
+package com.example.booknest.navigation
+
+sealed class Screen(val route: String) {
+    object Landing : Screen("landing")
+    object Login : Screen("login")
+    object AccountType : Screen("account_type")
+    object PersonalInfo : Screen("personal_info")
+    object ProfileDetails : Screen("profile_details")
+    object Bio : Screen("bio")
+    object Genres : Screen("genres")
+    object Home : Screen("home")
+}
