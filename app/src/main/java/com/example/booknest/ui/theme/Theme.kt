17d520cc089323538940785407dc7ab5489dc752
Ilina Tsanova <ilina.tsanova@stamsoft.com>
2026-01-03 09:43:59 +0200

    fix: constants and theme

--- app/src/main/java/com/example/booknest/ui/theme/Theme.kt
+++ app/src/main/java/com/example/booknest/ui/theme/Theme.kt
@@ -9,34 +9,40 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = SkyBluePeriwinkle,
+    secondary = LightBlueGray,
+    tertiary = LightGray,
+    background = VeryDarkNavy,
+    surface = DarkTealSlate,
+    onPrimary = Color.White,
+    onSecondary = VeryDarkNavy,
+    onTertiary = VeryDarkNavy,
+    onBackground = Color.White,
+    onSurface = Color.White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    primary = DarkNavyBlue,
+    secondary = SkyBluePeriwinkle,
+    tertiary = LightBlueGray,
+    background = BackgroundWhite,
+    surface = BackgroundWhite,
+    surfaceVariant = LightGray,
     onPrimary = Color.White,
     onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    onTertiary = DarkNavyBlue,
+    onBackground = DarkNavyBlue,
+    onSurface = DarkNavyBlue,
+    onSurfaceVariant = DarkNavyBlue
 )
 
 @Composable
 fun BookNestTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
